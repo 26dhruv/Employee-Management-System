08211ec740e889ae8eee89d453371227a3f6e709
26dhruv <dhruvkalpeshthakkar@gmail.com>
2023-07-27 02:12:39 -0400

    css pictures

---
+++ tempForForgPass.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Reset Email</title>
+  </head>
+  <body>
+    <table cellpadding="0" cellspacing="0" border="0" width="100%">
+      <tr>
+        <td style="background-color: #f1f1f1; padding: 20px;">
+          <h1 style="font-size: 24px; color: #333;">To Reset your password click below link</h1>
+          <p style="font-size: 16px; color: #666;">Reset Link : {Link}</p>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
